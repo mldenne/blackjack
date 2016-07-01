cf0c2a2be15a1d5fec10fe5c37258a99c08da5cb
Marie Denney <mldenne@gmail.com>
2016-06-30 21:48:27 -0400

    Added changes

--- blackjack.rb
+++ blackjack.rb
@@ -31,90 +31,77 @@ class Blackjack
   def player_action
     initial_deal
     puts "You are holding:"
-    player.each do |card|
-      puts "#{card.face} #{card.suit}"
-    end
-    total = 0
-    player.each do |value|
-      total += card.value
-    end
-      if total == 21
+    player.each {|card| puts "#{card.face} of #{card.suit}"}
+    puts "Dealer shows " #dealer.first #{card.face} of #{card.suit}
+    player_total
+  end
+
+  def player_total
+    player_total = player.inject(0){|sum, x| sum += x.value}
+    puts "Your total is #{player_total}"
+      if player_total == 21 # and only 2 cards
         puts "Blackjack"
+        # end game - show dealer cards, ask for new game
+      elsif player_total == 21 # with 3 cards
+        puts "You reached 21"
+      elsif player_total > 21
+        puts "Bust" #need method for Bust
       else
-        puts "Hit or Stay (H/S)?"
+        puts "Hit or Stay (h/s)?"
         response = gets.chomp.downcase
         if response == "h"
           player_hit
         else
-          stay
+          dealer_action
         end
       end
-    puts "Hit or Stay (H/S)?"
-    response = gets.chomp.downcase
-    if response == "h"
-      player_hit
-    else
-      stay
-    end
   end
 
   def player_hit
     @player << deck.deal
-    player.each do |card|
-      puts "#{card.face} #{card.suit}"
-    end
-    player.each do |value|
-      total += value
-    end
-      if total == 21
-        puts "Blackjack"
-      else
-        puts "Hit or Stay (H/S)?"
-        response = gets.chomp.downcase
-        if response == "h"
-          player_hit
-        else
-          stay
-        end
-      end
-  end
-
-  def stay
-    dealer_action
+    player.each {|card| puts "#{card.face} of #{card.suit}"}
+    player_total
   end
 
   def dealer_action
     puts "Dealers cards are:"
-    dealer.each do |car|
-      puts "#{card.face} #{card.face}"
+    dealer.each {|card| puts "#{card.face} of #{card.suit}"}
+    dealer_total
+  end
+
+  def dealer_total
+    dealer_total = dealer.inject(0){|sum, x| sum += x.value}
+    puts "Dealers total is #{dealer_total}"
+    if dealer_total == 21 && dealer_total > player_total
+      puts "Blackjack, dealer wins"
+    elsif dealer_total > player_total
+      puts "Dealer wins"
+    elsif dealer_total < 16 && dealer_total < player_total
+      dealer_hit
+    else
+      "Bust"
     end
-    if card_total.dealer == 21
-      "Blackjack"
-    elsif card_total.dealer < 21
-      hit
+  end
+
+  def dealer_hit
+    @dealer << deck.deal
+    dealer.each {|card| puts "#{card.face} of #{card.suit}"}
+    dealer_total = dealer.inject(0){|sum, x| sum += x.value}
+    if dealer_total <= 21 && dealer_total > player_total
+      puts "Dealer wins"
+    elsif dealer_total > player_total
+      puts "Dealer wins"
+    elsif dealer_total < 16 && dealer_total < player_total
+      dealer_hit
     else
       "Bust"
     end
   end
 
   def outro
-    if
-      puts "#{winner} wins!"
-    else
-      puts "Bust!"
-    end
-  end
-
-  def winner
-    if card_total.player < card_total.dealer
-      "Dealer"
-    elsif card_total.dealer < card_total.player
-      "Player"
-    else
-      bust
-    end
   end
 
 end
+
 
 Blackjack.new.play
